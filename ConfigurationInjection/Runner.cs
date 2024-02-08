897056916117bf8a06e8e00c251ca59787c59b82
Soonil Hong <soonil.hong@enable.com>
2024-02-07 21:34:33 -0500

    Use HelloService to say hello in Runner

--- ConfigurationInjection/Runner.cs
+++ ConfigurationInjection/Runner.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using ConfigurationInjection.Services;
+using Microsoft.Extensions.Hosting;
 
 namespace ConfigurationInjection;
 
 public class Runner : IHostedService
 {
+    private readonly IHelloService _helloService;
+
+    public Runner(IHelloService helloService)
+    {
+        _helloService = helloService;
+    }
+
     public async Task StartAsync(CancellationToken cancellationToken)
     {
+        _helloService.SayHello();
         Console.WriteLine($"[{nameof(Runner)}.{nameof(StartAsync)}()][{DateTime.Now:yyyy-MM-dd HH:mm:ss}]");
     }
 
