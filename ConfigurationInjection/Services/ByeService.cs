5430de4af2ebf2f69e34b093821de74bc1835110
Soonil Hong <soonil.hong@enable.com>
2024-02-07 21:57:46 -0500

    Inject configuration to services

--- ConfigurationInjection/Services/ByeService.cs
+++ ConfigurationInjection/Services/ByeService.cs
@@ -1,9 +1,19 @@
-﻿namespace ConfigurationInjection.Services;
+﻿using ConfigurationInjection.Config;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationInjection.Services;
 
 public class ByeService : IByeService
 {
+    private readonly ServiceConfiguration _configuration;
+
+    public ByeService(IOptions<ServiceConfiguration> options)
+    {
+        _configuration = options.Value;
+    }
+
     public void SayBye()
     {
-        Console.WriteLine($"Bye from {nameof(ByeService)}");
+        Console.WriteLine($"Bye from {nameof(ByeService)}. I love you {_configuration.HowMuchLove}");
     }
 }
--- ConfigurationInjection/Services/HelloService.cs
+++ ConfigurationInjection/Services/HelloService.cs
@@ -1,9 +1,19 @@
-﻿namespace ConfigurationInjection.Services;
+﻿using ConfigurationInjection.Config;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationInjection.Services;
 
 public class HelloService : IHelloService
 {
+    private readonly ServiceConfiguration _configuration;
+
+    public HelloService(IOptions<ServiceConfiguration> options)
+    {
+        _configuration = options.Value;
+    }
+
     public void SayHello()
     {
-        Console.WriteLine($"Hello from {nameof(HelloService)}");
+        Console.WriteLine($"Hello from {nameof(HelloService)} - {_configuration.ServiceName}");
     }
 }
