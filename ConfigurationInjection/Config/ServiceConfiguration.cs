7d29c02ff18b7ba9517917f8b27a32348d0a3531
Soonil Hong <soonil.hong@enable.com>
2024-02-07 21:51:11 -0500

    Add and register configuration

---
+++ ConfigurationInjection/Config/ServiceConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace ConfigurationInjection.Config;
+
+public record ServiceConfiguration
+{
+    public required string ServiceName { get; init; }
+
+    public required TimeSpan ImportJobInterval { get; init; }
+
+    public required string Password { get; init; }
+
+    public required int HowMuchLove { get; init; }
+}
--- ConfigurationInjection/Program.cs
+++ ConfigurationInjection/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ConfigurationInjection;
+using ConfigurationInjection.Config;
 using ConfigurationInjection.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +12,10 @@ builder.Services.AddHostedService<Runner>();
 // add service dependencies
 builder.Services.AddTransient<IHelloService, HelloService>();
 builder.Services.AddTransient<IByeService, ByeService>();
+
+// configuration
+var configuration = builder.Configuration.GetRequiredSection("ServiceConfiguration");
+builder.Services.Configure<ServiceConfiguration>(configuration);
 
 var host = builder.Build();
 await host.RunAsync();
